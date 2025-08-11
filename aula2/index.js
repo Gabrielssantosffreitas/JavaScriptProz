c6b73380cecc8ff086cda449a72fe81e40d2f15c
Gabriel Santos <gabrielsantos022freitas@gmail.com>
2025-08-11 19:34:52 -0300

    aula2

---
+++ aula2/index.js
@@ -0,0 +1,28 @@
+class Calculadora{
+
+    static printarResultado(resultado){
+        console.log(`O resultado é: ${resultado}`);
+    }
+
+    static somar(a, b){
+        return a + b;
+    }
+
+    getNumber(){
+        return parseFloat(prompt("Digite um número:"));
+    }
+    static main(){
+        const calculadora = new Calculadora();
+        const num1 = calculadora.getNumber();
+        const num2 = calculadora.getNumber();
+        const resultado = this.somar(num1, num2);
+        this.printarResultado(resultado);
+    }
+    
+
+
+}
+
+Calculadora.main();
+
+
