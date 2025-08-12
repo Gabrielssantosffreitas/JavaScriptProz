2d37b4f8c6a74110b690b8635e3ce58a4ef79d1a
Gabriel <161156836+Gabrielssantosffreitas@users.noreply.github.com>
2025-08-12 00:41:05 +0000

    atividade aula 03

---
+++ aula03/index.js
@@ -0,0 +1,16 @@
+class Index {
+    static main(){
+        const N1 = 10;
+        const N2 = 5; 
+        let soma = N1;
+        soma += N2;
+        console.log(soma);
+
+        const NOME = "gabriel";
+        const SOBRENOME = "santos"
+
+        console.log("bem vindo! "+NOME +" "+ SOBRENOME);
+    }
+}
+
+Index.main()
